049e886d85150a1cb0712486e511fd0f813e75c0
Edoo29 <edoardo.zaffaroni@gmail.com>
2025-02-03 22:06:57 +0100

    removed types.ts file

--- src/app/types.ts
+++
@@ -1,7 +0,0 @@
-interface LoadTransition {
-  initial: { opacity: number; y: number };
-  animate: { opacity: number; y: number };
-  transition: { duration: number; delay?: number };
-}
-
-export type { LoadTransition };
