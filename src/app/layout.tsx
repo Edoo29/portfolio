78cc7ceeb18cb97eaee1df5779d4792eb184d864
Edoo29 <edoardo.zaffaroni@gmail.com>
2025-02-10 21:04:41 +0100

    test commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import { roboto } from "./fonts";
-import Navbar from "./_components/navbar";
 
 export const metadata: Metadata = {
   title: "Portfolio",
